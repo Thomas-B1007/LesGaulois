1d2b1d233fae9b8c8b86bc1213a113646eb7a6f3
Thomas-B1007 <thomas.bjn65@gmail.com>
2025-11-03 19:45:04 +0100

    tp3

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -26,34 +26,89 @@ public class Romain {
 		return "Le Romain " + nom + " : ";
 	}
 
-	public void recevoirCoup(int forceCoup) {
-		assert (forceCoup >= 0);
-		int temp = force;
-		force = force - forceCoup;
-		if (force <= 0) {
-			parler("J'abandonne !");
-		} else {
-			parler("Aïe !");
+//	public void recevoirCoup(int forceCoup) {
+//		assert (forceCoup >= 0);
+//		int temp = force;
+//		force = force - forceCoup;
+//		if (force <= 0) {
+//			parler("J'abandonne !");
+//		} else {
+//			parler("Aïe !");
+//		}
+//		assert (temp > force);
+//		assert isInvariantVerified();
+//	}
+	
+	public Equipement[] recevoirCoup(int forceCoup) {
+		Equipement[] equipementEjecte = null;
+		forceCoup = CalculResistanceEquipement(forceCoup);
+		force -= forceCoup;
+		switch (force) {
+		case 0:
+			parler("Aïe");
+			break;
+		default:
+			equipementEjecte = ejecterEquipement();
+			parler("J'abandonne...");
+			break;
 		}
-		assert (temp > force);
-		assert isInvariantVerified();
+		return equipementEjecte;
 	}
+	
+	private int CalculResistanceEquipement(int forceCoup) {
+		texte = "Ma force est de " + this.force + ", et la force du coup est de" + forceCoup;
+		int resistanceEquipement = 0;
+		if (!(nbEquipement == 0)) {
+			texte += "\nMais heureusement, grace à mon équipement sa force est diminué de ";
+			for (int i = 0; i < nbEquipement;) {
+				if ((equipements[i] != null &&
+						equipements[i].equals(Equipement.BOUCLIER)) == true) {
+					resistanceEquipement += 8;
+				} else {
+					System.out.println("Equipement casque");
+					resistanceEquipement += 5;
+				}
+				i++;
+			}
+			texte =+ resistanceEquipement + "!";
+		}
+		parler(texte);
+		forceCoup -= resistanceEquipement;
+		return forceCoup;
+	}
+	
+	private Equipement[] ejecterEquipement() {
+		Equipement[] equipementEjecte = new Equipement[nbEquipement];
+		System.out.println("L'équipement de " + nom.toString() + " s'envole sous la force du coup.");
+		//TODO
+		int nbEquipementEjecte = 0;
+		for (int i = 0; i < nbEquipement; i++) {
+			if (equipements[i] == null) {
+				continue;
+			} else {
+				equipementEjecte[nbEquipementEjecte] = equipements[i];
+				nbEquipementEjecte++;
+				equipements[i] = null;
+			}
+		}
+		return equipementEjecte;
+	}
+
+
 	
 	public void sEquiper(Equipement equipement) {
 		String debutPhrase = "Le soldat ";
 		switch (nbEquipement) {
-		case 1: {
+		case 1: 
 			if (equipement.equals(equipements[0])) {
 				System.out.println(debutPhrase + nom + " possède déjà un " + equipement + " !");
 			} else {
 				equiperEquipement(equipement, debutPhrase);
 			}
 			break;
-		}
-		case 0: {
+		case 0: 
 			equiperEquipement(equipement, debutPhrase);
 			break;
-		}
 		default:
 			System.out.println(debutPhrase + nom + " est déjà bien protégé !");
 		}
